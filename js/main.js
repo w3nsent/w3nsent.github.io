63d778ed9384eb4c1ba58b7d2e3e9378b63d8040
w3nsent <realwensent@mail.ru>
2018-05-27 09:58:54 +0300

    1
    1

--- js/main.js
+++
@@ -1,5 +0,0 @@
-$( document ).ready(function() {
-    particlesJS.load('overlay', '/media/particlesjs-config.json', function() {
-  		console.log('callback - particles.js config loaded');
-	});
-});
